a092aceb73355105a9a400f575f85ec9a2422b13
Luis Penaloza <ldp231@yahoo.com>
2020-09-14 22:25:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   count = 0 
   while count < array.length do 
-  new_array.push(array[lenght]**2)
+  new_array.push(array[length]**2)
   count += 1 
   end 
   return new_array
